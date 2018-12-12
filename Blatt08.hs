5c3ef4d49e11cda1e4be0ec1f70f8e18827f5c0f
louipivo <louidyusembaeva@gmail.com>
2018-12-12 10:17:45 +0100

    Exercisesheet 8

--- Blatt08.hs
+++ Blatt08.hs
@@ -10,47 +10,42 @@ data Edge = Links | Rechts deriving Show
 type Node = [Edge]
 
 -- Aufgabe 8.1 a)
-{-drop 0 s = s
-drop n (_:s) | n > 0 = drop (n-1) s
-drop _ [] = [] -}
-
 class GenDrop a where genDrop :: a -> [b] -> [b]
 -- Aufgabe 8.1 b)
 instance GenDrop (Int) where
-	genDrop 0 s = s
-	genDrop n (_:s) | n > 0 = genDrop (n-1) s
-	genDrop _ [] = []
+  genDrop 0 s = s
+  genDrop n (_:s) | n > 0 = genDrop (n-1) s
+  genDrop _ [] = []
 instance GenDrop (Nat) where
-	genDrop Zero s = s
-	genDrop (Succ n) (_:s) = genDrop n s
-	genDrop _ [] = []
+  genDrop Zero s = s
+  genDrop (Succ n) (_:s) = genDrop n s
+  genDrop _ [] = []
 instance GenDrop (PosNat) where
-	genDrop One (_:s) = s
-	genDrop (Succ' n) (_:s) = genDrop n s
-	genDrop _ [] = []
+  genDrop One (_:s) = s
+  genDrop (Succ' n) (_:s) = genDrop n s
+  genDrop _ [] = []
 instance GenDrop (Int') where
-	genDrop Zero' s = s
-	genDrop (Plus (Succ' n)) (_:s) = genDrop n s
-	genDrop (Minus (Succ' n))(_:s) = genDrop n s
-	genDrop _ [] = []
+  genDrop Zero' s = s
+  genDrop (Plus (Succ' n)) (_:s) = genDrop n s
+  genDrop (Minus (Succ' n))(_:s) = genDrop n s
+  genDrop _ [] = []
 
 -- Aufgabe 8.2 a)
-
---map fromEnum [Minus One .. Plus (Succ' One)] { [-1,0,1,2]
 instance Enum Int' where
   toEnum  0 = Zero'
   toEnum n | n > 0 = (Plus (toEnum1 (n)))
-           | n < 0 = (Minus (toEnum1 (-n)))
-		   where
-	toEnum1:: Int -> PosNat
-	toEnum1 1 = One
-	toEnum1 n = Succ' (toEnum1(n-1))
-	
-  --fromEnum One = 1
-  
-  --fromEnum (Plus (Succ' n)) = (fromEnum n) + 1
-  --fromEnum (Minus (Succ' n)) = (fromEnum n) - 1
-{-
+           | n < 0 = (Minus (toEnum1 (-n))) where
+    toEnum1 :: Int -> PosNat
+    toEnum1 1 = One
+    toEnum1 n = Succ' (toEnum1(n-1))
+  fromEnum Zero' = 0
+  fromEnum (Plus n) = fromEnum1 n
+  fromEnum (Minus n) = - (fromEnum1 n)
+
+fromEnum1 :: PosNat -> Int
+fromEnum1 One = 1
+fromEnum1 (Succ' n) = fromEnum1 n + 1
+
 -- Aufgabe 8.2 b)
 instance Num Int' where
   fromInteger = toEnum . fromInteger
@@ -60,21 +55,12 @@ instance Num Int' where
   abs n = n
   (+) n m = toEnum(fromEnum(n) + fromEnum(m))
   (*) n m = toEnum(fromEnum(n) * fromEnum(m))
-  --(+) Zero' n = n
-  {-(Succ' n) (+) m = Succ' (n + m)
-  Zero' (+) n = n
-  (Succ' n) (*) m = m + (n*m)
-  Zero' (*) n = Zero'
--}
+
 instance Eq Int' where
-  -- (==) Zero' Zero' = True
-  -- (==) (Succ' m) (Succ' n) = fromEnum(n) == fromEnum(m)
-  -- (==) _ _ = False
+  (==) n m = fromEnum(n) == fromEnum(m)
 
 instance Ord Int' where
-  -- Succ' n (<=) Succ' m = n <= m
-  -- Zero' (<=) _ = True
-  -- _ (<=) _ = False
+  (<=) n m = fromEnum(n) <= fromEnum(m)
 
 instance Show Int' where
   show = show . fromEnum
@@ -84,23 +70,20 @@ instance Show Int' where
 -- Kann durch die eigene Lösung ersetzt werden.
 -- Ändern Sie den Typ in [(Int',Int',Int')].
 solutions :: [(Int',Int',Int')]
-solutions = [ (x,y,z) | z <- [0..] , x <- [0..z] , y <- [0..z]
+solutions = [ (toEnum(x), toEnum(y), toEnum(z)) | z <- [0..] , x <- [0..z] , y <- [0..z]
             , 5*x + 3*y^2 + 10 == z ]
 
--}
 -- Aufgabe 8.3 a)
-{-data Bintree a = Empty | Fork a (Bintree a) (Bintree a)
-leaf :: a -> Bintree a
-leaf a = Fork a Empty Empty-}
 sizeBintree :: Bintree a -> Int
 sizeBintree (Fork a b c) = (sizeBintree b) + (sizeBintree c) + 1
 sizeBintree (Empty) = 0
 
-
 -- Aufgabe 8.3 b)
 zipBintree :: Bintree a -> Bintree b -> Bintree (a,b)
-zipBintree = undefined -- Durch Lösung ersetzen.
+zipBintree _ Empty = Empty
+zipBintree Empty _ = Empty
+zipBintree (Fork a1 l1 r1) (Fork a2 l2 r2) = Fork(a1, a2) (zipBintree l1 l2) (zipBintree r1 r2)
 
 -- Aufgabe 8.3 c)
 getSubbintree :: Bintree a -> Node -> Maybe (Bintree a)
-getSubbintree = undefined -- Durch Lösung ersetzen.
+getSubbintree = undefined
